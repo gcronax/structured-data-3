56f1fcd7a0eecfab99af141287d5b8099491a6a9
gcronax <guillecampher15@gmail.com>
2025-01-16 20:19:59 +0100

    qweqw

--- src/maps_6.java
+++ src/maps_6.java
@@ -1,31 +1,51 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class maps_6 {
     public static void main(String[] args) {
-        String csvFile = "apellidos.csv";
-        BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ",";
+        Map<String, String> aux_map = new HashMap<String, String>();
+
+        BufferedReader bufferLectura = null;
         try {
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-                String[] datos = line.split(cvsSplitBy);
-                //Imprime datos.
-                System.out.println(datos[0] + ", " + datos[1] + ", " + datos[2] + ", " + datos[3] + ", " + datos[4] + ", " + datos[5]);
+            // Abrir el .csv en buffer de lectura
+            bufferLectura = new BufferedReader(new FileReader("apellidos.csv"));
+
+            // Leer una linea del archivo
+            String linea = bufferLectura.readLine();
+
+            while (linea != null) {
+                // Sepapar la linea leída con el separador definido previamente
+                String[] campos = linea.split(",");
+
+                //System.out.println(Arrays.toString(campos));
+                String i=campos[0];
+                String j= campos[1];
+
+
+                    aux_map.put(i, j);
+
+
+                // Volver a leer otra línea del fichero
+                linea = bufferLectura.readLine();
             }
-        } catch (FileNotFoundException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
+        }
+        finally {
+            // Cierro el buffer de lectura
+            if (bufferLectura != null) {
                 try {
-                    br.close();
-                } catch (IOException e) {
+                    bufferLectura.close();
+                }
+                catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+        System.out.println(aux_map);
     }
 
 }
